26c83c7993ac6f47d7fc3c5faf324f62c770eb8c
LuthfiIzzudinRahman <luthfiizzudinr6@gmail.com>
2023-08-28 17:02:11 +0700

    first commit

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend_app.Models
+{
+
+    public class Employee
+    {
+        public int id { get; set; }
+        public int Name { get; set; }
+        public int PhoneNo { get; set; }
+        public int Address { get; set; }
+        public int IsActive { get; set; }
+    }
+}
